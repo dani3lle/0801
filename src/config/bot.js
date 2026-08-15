558f6265f0396bae8f6db92faf09cea4db16aff7
dani3lle <84687628+dani3lle@users.noreply.github.com>
2026-08-15 04:34:48 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "❀ You're doing great, okay? Blooming in the rain~",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
